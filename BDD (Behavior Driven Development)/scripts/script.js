cdb9a2824fc9cad7bb69d61be3a48f5649b8de54
x45jk <student_802@mail.ru>
2018-04-14 19:24:38 +0300

    18 days

---
+++ BDD (Behavior Driven Development)/scripts/script.js
@@ -0,0 +1,9 @@
+function pow(x, n) {
+    var result = 1;
+  
+    for (var i = 0; i < n; i++) {
+      result *= x;
+    }
+  
+    return result;
+}
--- testSite/scripts/script.js
+++ testSite/scripts/script.js
@@ -1,12 +1,24 @@
-var form = document.querySelector('form');
-var fname = document.getElementById('fname');
-var lname = document.getElementById('lname');
-var submit = document.getElementById('submit');
-var para = document.querySelector('p');
-
-form.onsubmit = function(e) {
-  if (fname.value === '' || lname.value === '') {
-    e.preventDefault();
-    para.textContent = 'You need to fill in both names!';
-  }
+function pow(x, n) {
+    var result = 1;
+  
+    for (var i = 0; i < n; i++) {
+      result *= x;
+    }
+  
+    return result;
 }
+
+function arrFunction() {
+  var array = new Array(4);
+  array[0] = 'a';
+  array[4] = 'b';
+  array[7] = 7;
+
+  console.log(array);
+}
+
+var arr = ["Я", "изучаю", "JavaScript"];
+
+var mass = arr.splice(1, 1); // начиная с позиции 1, удалить 1 элемент
+
+alert( mass ); //  осталось ["Я", "JavaScript"]
